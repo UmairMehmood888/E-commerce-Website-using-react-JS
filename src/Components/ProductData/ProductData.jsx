c2dcf3a9d8f4e048b26f2884ea7815d9901ea9d3
unknown <ch.umairmehmood.pk@gmail.com>
2024-05-18 15:05:57 +0500

    add files

---
+++ src/Components/ProductData/ProductData.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import Popular from '../Popular/Popular';
+
+const ProductData = () => {
+
+    // // const [dummyData, setDummyData] = useState([null]);
+    // // console.log(dummyData);
+
+    // // useEffect(() => {
+    // //     const fetchData = async () => {
+    // //         try {
+    // //             const response = await fetch("https://fakestoreapi.com/products");
+    // //             const jsonData = await response.json();
+    // //             setDummyData(jsonData);
+    // //         }
+    // //         catch (error) {
+    // //             console.error("Error fecting data:", error);
+    // //         }
+    // //     };
+    // //     fetchData();
+    // // }, []);
+
+    // // return (
+    // //     <div>
+    // //         {dummyData && <Popular dummyData={dummyData}/>}
+    // //     </div>
+    // // );
+};
+
+export default ProductData;
